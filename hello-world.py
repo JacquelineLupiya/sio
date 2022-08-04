660c19483cb17edd1fbce34b91016e3e67234dd5
Jacqueline Lupiya <60430394+Jyakuriin@users.noreply.github.com>
2022-08-04 13:52:33 +0200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -2,3 +2,5 @@ print("My name is Vikwato Kamanga.\n"
       "I am currently pursuing a Masters of Science Degree in Analytics from Georgia Insititute of Technology\n"
       "I enjoy programming, data management and system administration\n"
       "Outside work, I enjoy watching and playing sport, listening to music, playing video games and watching TV.")
+print("my name is Jacqueline Lupiya, 22 years of age. \n"
+"currently taking the intern quiz")
